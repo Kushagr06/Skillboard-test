785675e41e64e38157e675a9ff4277bc44d7cf1e
Kushagr Narayan <nkushagr@gmail.com>
2022-12-11 00:56:31 +0530

    carasoul left

--- src/components/Caras.js
+++ src/components/Caras.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Caras() {
   return (
-    <div className='bg-green-600'>Caras</div>
+    <div className=' md:w-1/4 bg-teal-900 '>
+    </div>
   )
 }
