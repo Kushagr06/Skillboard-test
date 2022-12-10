16078205a8624916c162bfa67b9ea203d06a8dd4
Kushagr Narayan <nkushagr@gmail.com>
2022-12-11 00:18:16 +0530

    almsot done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import Caras from './components/Caras';
 import Form from './components/Form';
 
 function App() {
   return (
     <>
-    <div className="App bg-red-400">
+    <div className="flex flex-row">
+      
+        
+      <Caras />
       < Form />
+      
       </div>
     </> 
   );
---
+++ src/components/Caras.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Caras() {
+  return (
+    <div className='bg-green-600'>Caras</div>
+  )
+}
